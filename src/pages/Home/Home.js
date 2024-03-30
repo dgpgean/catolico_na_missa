6b1f019791a78d1a51dcc48f57fe6161c53cdae4
dgpgean <52607904+dgpgean@users.noreply.github.com>
2024-03-30 16:16:25 -0300

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,9 +4,37 @@ import Services from '../../components/Services';
 const Home = () => {
   return (
     <div className='container '>
-      <Services />
-    </div>
-  )
-}
+  Política de Privacidade para Aplicativo Android que Não Coleta Informações de Usuário
+1. Introdução
+
+Esta Política de Privacidade ("Política") descreve como coletamos, usamos e compartilhamos seus dados pessoais quando você usa nosso aplicativo Android ("Católico na missa").
+
+2. Informações coletadas
+
+2.1 Informações que você fornece
+
+Este aplicativo não coleta nenhuma informação pessoal diretamente de você.
+
+2.2 Informações coletadas automaticamente
+
+Este aplicativo não coleta nenhuma informação pessoal.
+
+3. Uso das informações
+
+  Não utilizamos nenhuma informação do usuário.
+
+4. Mudanças na Política
+
+Podemos atualizar esta Política periodicamente. A versão mais recente da Política estará sempre disponível no Aplicativo.
+
+5. Contato
+
+Se você tiver dúvidas ou preocupações sobre esta Política, entre em contato conosco pelo email dgpgran@gmail.com.
+
+6. Data de vigência
+
+Esta Política entra em vigor em 01/04/2024.
+
+
 
 export default Home
